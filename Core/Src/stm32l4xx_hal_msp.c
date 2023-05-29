fde611b4bff5f5938dc412c59ef1607033a952e3
SAMGO994 <Samuele.gobbi@heifr.ch>
2023-05-29 20:33:43 +0200

    Deleted all stuff concerning PWM, now it doesn't fall into hard fault

--- Core/Src/stm32l4xx_hal_msp.c
+++ Core/Src/stm32l4xx_hal_msp.c
@@ -59,7 +59,7 @@
 /* USER CODE END 0 */
 
 void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
-                    /**
+                                        /**
   * Initializes the Global MSP.
   */
 void HAL_MspInit(void)
